7c6d047c865ac8a001c6613b385dab2e2a2ee137
saffrun <104745771+saffrun@users.noreply.github.com>
2024-02-29 17:11:27 -0500

    Converted frontend html to react and added an image

---
+++ frontend/reddit-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginForm from './assets/log-in-sign-up/log-in-sign-up';
+import LoveSosaImage from './assets/image/Love Sosa.png';
+
+function App() {
+  return (
+    <div>
+      <img src={LoveSosaImage} alt="Love Sosa" />
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
