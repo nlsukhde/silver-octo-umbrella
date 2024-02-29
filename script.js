694325b3b51256d01a6dbdfd68668c106966a7df
karthikkdub <62803571+karthikkdub@users.noreply.github.com>
2024-02-29 17:21:29 -0500

    Update two functions for login and signup button

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSignUp() {
+    document.getElementById('login-form').style.display = 'none';
+    document.getElementById('signup-form').style.display = 'block';
+}
+
+function showLogin() {
+    document.getElementById('signup-form').style.display = 'none';
+    document.getElementById('login-form').style.display = 'block';
+}
