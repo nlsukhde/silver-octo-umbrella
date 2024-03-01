126e60376a8943f7e986058c064026930988d817
saffrun <104745771+saffrun@users.noreply.github.com>
2024-02-29 19:28:09 -0500

    log-in-sign-up.jsx to src

--- frontend/reddit-app/src/App.jsx
+++ frontend/reddit-app/src/App.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import LoginForm from './assets/log-in-sign-up/log-in-sign-up';
+import LoginForm from './log-in-sign-up';
 import LoveSosaImage from './assets/image/Love Sosa.png';
 
 function App() {
