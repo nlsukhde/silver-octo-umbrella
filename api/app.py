37c9c5fec5cd72f0f1aa55c597f5697d79545e92
nlsukhde <102875950+nlsukhde@users.noreply.github.com>
2024-04-21 20:17:35 -0400

    Update app.py

--- api/app.py
+++ api/app.py
@@ -302,7 +302,8 @@ def comment_post(post_id):
     return jsonify({'message': 'Comment successfully'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)  
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 
